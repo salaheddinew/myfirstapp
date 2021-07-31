2ad8db6aba1f8b8c984d348a097acd255433e1c3
salah <heba.salah400@gmail.com>
2021-07-31 18:09:57 +0100

    gg1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App btn-salah">
-      <p className="">Join the healthier snaks </p>
-      <button className="btn btn-warning ">Daily musly Family</button>
+    // <div className="App btn-salah">
+    //   <p className="">Join the healthier snaks </p>
+    //   <button className="btn btn-warning ">Daily musly Family</button>
+    // </div>
+    <div style={{ border: "solid 1px", color: "black", maxWidth: "100vw" }}>
+      <h1 class={"title red"}>Your name here</h1>
+
+      <br />
+
+      <img src={logo} />
+
+      <br />
+
+      <img src="/logo192.png" />
+
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
